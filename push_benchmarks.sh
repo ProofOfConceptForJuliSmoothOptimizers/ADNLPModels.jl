8a2370ab00685606dbf28b9e105ff831b1620917
MonssafToukal <32516773+MonssafToukal@users.noreply.github.com>
2021-08-11 17:02:21 -0400

    Update push_benchmarks.sh

--- push_benchmarks.sh
+++ push_benchmarks.sh
@@ -12,7 +12,7 @@ fi
 julia benchmark/$1 $repo
 
 if [ "$?" -eq "0" ] ; then
-    julia benchmark/send_comment_to_pr.jl -o $org -r $repo -p $pullrequest -c "Benchmark results" -g 'gist.json'
+    julia benchmark/send_comment_to_pr.jl -o $org -r $repo -p $pullrequest -c "Benchmark results" -g gist.json
 else
     ERROR_LOGS="/home/jenkins/benchmarks/$org/$repo/${pullrequest}_bmark_error.log"
     julia benchmark/send_comment_to_pr.jl -o $org -r $repo -p $pullrequest -c "**An error occured while running the benchmark script $1**" -g ERROR_LOGS
