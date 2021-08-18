7efc2079bf1b75d0cc8a0992d306236a6d8af84e
Monssaf Toukal <tmonssaf@gmail.com>
2021-08-18 10:46:53 -0400

    Fix bug with checkout behaviour

--- push_benchmarks.sh
+++ push_benchmarks.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
-git checkout $BRANCH_NAME --
 
 julia benchmark/send_comment_to_pr.jl -o $org -r $repo -p $pullrequest -c '**Starting benchmarks!**'
 
-if [ "$?" -ne "0" ] ; then
-    LOCAL_BRANCH_NAME="temp_bmark"
-    git fetch origin pull/$pullrequest/head:$LOCAL_BRANCH_NAME
-    git checkout $LOCAL_BRANCH_NAME --
-fi
+LOCAL_BRANCH_NAME="temp_bmark"
+git fetch origin pull/$pullrequest/head:$LOCAL_BRANCH_NAME
+git checkout $LOCAL_BRANCH_NAME --
 
 julia benchmark/$1 $repo
 
